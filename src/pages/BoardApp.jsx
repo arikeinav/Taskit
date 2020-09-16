65c182e2bded1961e05ba2fa94a05e4e320ccf09
Shlomikop <64078705+Shlomikop@users.noreply.github.com>
2020-09-16 15:38:55 +0300

    master

--- src/pages/BoardApp.jsx
+++ src/pages/BoardApp.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+
 import {BoardList} from '../cmps/BoardList'
+import {React}  from 'react'
+import { connect } from 'react-redux'
 
 
-export class BoardApp extends React.Component {
+
+class _BoardApp extends React.Component {
 
     render(){
     return (
         <div className="board-app">
             <h1>Most popular templates</h1>
-            <BoardList />
+            <BoardList boards={this.props.boards}  />
 
 
         </div>
     )
     }
 }
+
+const mapStateToProps = (state) => ({
+    boards:state.boardReducer.boards
+})
+
+const mapDispatchToProps = {
+    
+}
+export const BoardApp=  connect(mapStateToProps, mapDispatchToProps)(_BoardApp)
+
