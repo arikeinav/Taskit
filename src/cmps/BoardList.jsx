7e3c95f937e44d6a926cca8b217b6920cb045cb4
Shlomikop <64078705+Shlomikop@users.noreply.github.com>
2020-09-16 15:52:35 +0300

    board app

--- src/cmps/BoardList.jsx
+++ src/cmps/BoardList.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import BoardPreview from './BoardPreview.jsx'
 
-export  function BoardList(boards, onRemove) {
+export  function BoardList(boards) {
+    console.log(boards);
     return (
+        
         <div>
-           { boards.map(board => <BoardPreview onRemove={onRemove} board={ board } key={ board._id } />)}
+           { boards.map(board => <BoardPreview  board={ board } key={ board._id } />)}
         </div>
     )
 }
--- src/pages/BoardApp.jsx
+++ src/pages/BoardApp.jsx
@@ -1,30 +1,34 @@
-
-import {BoardList} from '../cmps/BoardList'
-import {React}  from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { BoardList } from '../cmps/BoardList'
+import { loadBoards } from '../store/actions/boardActions'
+
+export class BoardApp extends Component {
+
+    componentDidMount() {
+        this.props.loadBoards()
+    }
 
 
+    render() {
+        return (
+            <div className="board-app">
+                <h1>Most popular templates</h1>
+                <BoardList boards={this.props.boards} />
+            </div>
+        )
 
-class _BoardApp extends React.Component {
-
-    render(){
-    return (
-        <div className="board-app">
-            <h1>Most popular templates</h1>
-            <BoardList boards={this.props.boards}  />
-
-
-        </div>
-    )
     }
 }
 
 const mapStateToProps = (state) => ({
-    boards:state.boardReducer.boards
+    boards: state.boardReducer.boards
 })
 
 const mapDispatchToProps = {
-    
+    loadBoards
 }
-export const BoardApp=  connect(mapStateToProps, mapDispatchToProps)(_BoardApp)
+
+export default connect(mapStateToProps, mapDispatchToProps)(BoardApp)
+
 
