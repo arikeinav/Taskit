b5fd2cfe6c4d0c25c542f0171ff58f6399d213dd
Shahar Sadof <shaharsadof22@gmail.com>
2020-09-16 15:23:57 +0300

    5555

--- src/cmps/BoardHeader.jsx
+++ src/cmps/BoardHeader.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+// import Avatar from '@material-ui/core/Avatar';
+
+
 export class BoardHeader extends Component {
+    
+    state = {
+        bord: {}
+    }
+
+    async componentDidMount() {
+        const bord = await require('../data.json').board
+        console.log("BoardHeader -> componentDidMount -> bord", bord)
+        this.setState({bord})
+    }
+
+    
     render() {
         return (
-            <div className="board-header">
-                {/* <p>{board.title}</p> */}
+            <div className="board-header flex">
+            <p>gggg</p>
+                {/* <p>{this.state.board.title}</p> */}
+                <section className="avatar-members flex">
+                    {/* {this.state.board.members.map(member =>{
+                    <Avatar alt={member.userName} src={member.imgUrl}  />
+                })} */}
+                    {/* <Avatar alt="Shahar" src="" >SK</Avatar>
+                    <Avatar alt="Arik" src="" >SS</Avatar>
+                    <Avatar alt="Shlomi">AE</Avatar> */}
+
+                </section>
             </div>
         )
     }
