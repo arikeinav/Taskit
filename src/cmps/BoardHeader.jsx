109bf0f03d750897e3743932233bc3b88bd89c22
Shahar Sadof <shaharsadof22@gmail.com>
2020-09-16 13:16:35 +0300

    Update BoardHeader.jsx

--- src/cmps/BoardHeader.jsx
+++ src/cmps/BoardHeader.jsx
@@ -4,19 +4,20 @@ import { connect } from 'react-redux'
 export class BoardHeader extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="board-header">
+                <p>{board.title}</p>
             </div>
         )
     }
 }
 
-const mapStateToProps = (state) => ({
-    
-})
-
+const mapStateToProps = state => {
+    return {
+        board = state.boardReducer.currBoard
+    }
+}
 const mapDispatchToProps = {
-    
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(BoardHeader)
