7bd02dc6d6ec7fd0020824b2be07e19de6017b4e
arikeinav <68658683+arikeinav@users.noreply.github.com>
2020-09-16 14:44:58 +0300

    board service

---
+++ src/services/boardService.js
@@ -0,0 +1,39 @@
+
+import httpService from './httpService';
+const board = require('../data.json')
+
+export const boardService = {
+  query,
+  getById,
+  removeBoard,
+  save,
+};
+
+function getById(boardId) {
+    return board
+//   return httpService.get(`board/${boardId}`)
+}
+
+
+async function save(board) {
+ 
+  if (board._id) {
+    console.log('board')
+    return  httpService.put(`board/${board._id}`, board);
+  } 
+  else {
+ 
+  const addedBoard  =  httpService.post(`board`, board);
+  return  addedBoard
+}
+}
+function query(filterBy) {
+  var queryStr =''
+  if (filterBy)  queryStr = `?name=${filterBy.name}&type=${filterBy.type}&inStock=${filterBy.inStock}`;
+  return httpService.get(`board${queryStr|| ''}`);
+  
+}
+
+function removeBoard(boardId) {
+  return httpService.delete(`board/${boardId}`);
+}
