45dbad21b757e02663f9856e4782573fa02ec055
Shahar Sadof <shaharsadof22@gmail.com>
2020-09-17 20:37:06 +0300

    end day 2

--- src/cmps/CardPreview.jsx
+++ src/cmps/CardPreview.jsx
@@ -11,9 +11,9 @@ export function CardPreview({ card, updateState, groupId, index }) {
     return (
         <Draggable draggableId={card.id} index={index}>
             {(provided) => (
-                <div className="card" onClick={() => openCardDetails(card.id)} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                <div className="card-preview" onClick={() => openCardDetails(card.id)} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
                     <p>{card.title}</p>
-                    {card.imgUrl && <img className="img-caed-preview" src={card.imgUrl} alt="Loading"/>}
+                    {card.imgUrl && <img className="img-card-preview" src={card.imgUrl} alt="Loading"/>}
                 </div>
             )}
         </Draggable>
