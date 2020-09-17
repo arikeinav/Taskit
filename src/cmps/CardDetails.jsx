580cffe2fa33304baad74a0e1a338257fb1c1f94
Shahar Sadof <shaharsadof22@gmail.com>
2020-09-17 10:39:49 +0300

    asdf

--- src/cmps/CardDetails.jsx
+++ src/cmps/CardDetails.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
-import {boardService} from '../services/boardService'
+import { Avatar } from '@material-ui/core';
+import { AvatarGroup } from '@material-ui/lab';
+import { boardService } from '../services/boardService'
 
 export class _CardDetails extends Component {
 
-    async componentDidMount() {
-        const card = await boardService.getCardById(this.props.board ,this.props.groupId ,this.props.cardId)
-        console.log("componentDidMouaaaaaaaaaaaaaaaant -> card", card)
+    state = {
+        card: null
     }
-    
+
+    async componentDidMount() {
+        const card = await boardService.getCardById(this.props.board, this.props.groupId, this.props.cardId)
+        this.setState({ card })
+    }
+
 
 
     onRmoveModal = () => {
@@ -17,17 +22,34 @@ export class _CardDetails extends Component {
     }
 
     render() {
+        if (!this.state.card) return <div>Loading...</div>
+        const card = this.state.card
+        console.log("render -> card", card)
         return (
             <div className="card-modal">
                 <div className="empty-modal" onClick={this.onRmoveModal}></div>
                 <div className="details-modal">
+                    <header className="card-header flex space-between">
+                        <h3>{card.title}</h3>
+                        <button onClick={this.onRmoveModal}>X</button>
+                    </header>
+                    <button>Invit</button>
+                    <section className="avatar-members flex">
+                        <AvatarGroup max={3}>
+                            {card.assignedMembers.map(member => {
+                                console.log("render -> member", member)
+                                return member.imgUrl ?
+                                    <Avatar key={member._id} asrc={member.imgUrl}></Avatar>
+                                    :
+                                    <Avatar key={member._id} src={member.imgUrl}>{member.userName.substring(0, 1).toUpperCase()}{member.userName.substring(1, 2).toUpperCase()}</Avatar>
+                            }
+                            )}
+                        </AvatarGroup>
+                    </section>
                     <p>description:</p>
-                    <p>{this.props.description}</p>
-                    {/* {console.log("render -> style.bgColor", this.props.style.bgColor)}
-                    <p>{this.props.style.bgColor}</p>
-                    <p>{this.props.cardId}</p>
-                    <p>{this.props.cardId}</p> */}
-                    
+                    {/* <textarea>{card.description}</textarea> */}
+
+
                 </div>
             </div>
         )
@@ -36,8 +58,9 @@ export class _CardDetails extends Component {
 
 const mapStateToProps = state => {
     return {
-    board: state.boardReducer.currBoard
-}}
+        board: state.boardReducer.currBoard
+    }
+}
 const mapDispatchToProps = {
 
 }
