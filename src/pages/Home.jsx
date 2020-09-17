d4f17e0216727f79deb129e722a948364ca9c80b
Shlomikop <64078705+Shlomikop@users.noreply.github.com>
2020-09-17 15:06:07 +0300

    2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom'
 
 export function Home() {
     return (
-        <div className="home">
-            <h1>Welcome To Taskit!</h1>
+        <div className="home flex column align-center justify-center">
+            <h1>Welcome To Taskit</h1>
             <Link to="/board"><p>Start</p></Link>
         </div>
     )
