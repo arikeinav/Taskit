70e81ff8107fe38b578dbc45c98768c9ed5e2b46
Shlomikop <64078705+Shlomikop@users.noreply.github.com>
2020-09-17 14:22:22 +0300

    123123

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 
+
 export function Home() {
     return (
-        <div>
+        <div className="home">
             <h1>Welcome To Taskit!</h1>
             <Link to="/board"><p>Start</p></Link>
         </div>
