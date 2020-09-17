2f32f9894b2597ddf3995105c5b2607a3b6111ab
arikeinav <68658683+arikeinav@users.noreply.github.com>
2020-09-17 16:51:56 +0300

    ihj

--- src/cmps/Login.jsx
+++ src/cmps/Login.jsx
@@ -1,22 +1,72 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { login} from "../store/actions/userActions";
 
-export class Login extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+class _Login extends Component {
+  state = {
+    msg: "",
+    loginCred: {
+      username: "",
+      password: "",
+    },
+  };
+
+  loginHandleChange = (ev) => {
+    const { name, value } = ev.target;
+    this.setState((prevState) => ({
+      loginCred: {
+        ...prevState.loginCred,
+        [name]: value,
+      },
+    }));
+  };
+
+  doLogin = async (ev) => {
+    ev.preventDefault();
+    const { username, password } = this.state.loginCred;
+    if (!username || !password) {
+      return this.setState({ msg: "Please enter user/password" });
     }
+    const userCreds = { username, password };
+    this.props.login(userCreds);
+    this.setState({ loginCred: { username: "", password: "" } });
+  };
+
+  render() {
+    return (
+      <div className="test">
+        <h2>{this.state.msg}</h2>
+        <form onSubmit={this.doLogin}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.loginCred.username}
+            onChange={this.loginHandleChange}
+            placeholder="username"
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={this.state.loginCred.password}
+            onChange={this.loginHandleChange}
+            placeholder="Password"
+          />
+          <br />
+          <button>Login</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
-    
-})
-
+const mapStateToProps = (state) => {
+  return {
+    loggedInUser: state.userReducer.loggedInUser,
+  };
+};
 const mapDispatchToProps = {
-    
-}
+  login,
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export const Login = connect(mapStateToProps, mapDispatchToProps)(_Login);
--- src/cmps/NavBar.jsx
+++ src/cmps/NavBar.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { NavLink, withRouter } from 'react-router-dom';
+import {Login} from './Login'
+import {Submit} from './Submit'
+import {Modal} from './Modal'
 
-function _NavBar() {
+class _NavBar extends React.Component {
+
+    state ={
+        isLogedShow:false
+    }
+onIsLogedShow=()=>{
+    this.setState({isLogedShow:true})
+}
+    render(){
     return (
         <nav className="nav-bar grid align-center">
            <div className="navdiv-s"> <NavLink className="nav-item" to="/">Home</NavLink></div>
            <div className="navdiv-s"><NavLink className="nav-item" to="/board">Boards</NavLink></div>
            <div className="logo">Taskit</div>
            <div className="navdiv-s"> <NavLink className="nav-item" to="/board/b123">Test Board</NavLink></div>
-            {/* <NavLink className="nav-item" to="/board/bexample">Public Board</NavLink> */}
-            <div className="navdiv-s nav-item">Login</div>
+            <div className="navdiv-s nav-item" onClick={this.onIsLogedShow}>Login</div>
+            <div className="navdiv-s nav-item" onClick={this.onIsSubmitShow}>Submit</div>
+    {this.state.isLogedShow && <Login />}
+    {this.state.isLogedShow && <Submit />}
+
         </nav>
     )
-}
+}}
 
 export const NavBar = withRouter(_NavBar)
