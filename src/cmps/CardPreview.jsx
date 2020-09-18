ee2de3fdbbe1a155aad1646ad85404dc836bb58c
arikeinav <68658683+arikeinav@users.noreply.github.com>
2020-09-18 15:50:05 +0300

    sc

--- src/cmps/CardPreview.jsx
+++ src/cmps/CardPreview.jsx
@@ -12,7 +12,7 @@ export function CardPreview({ card, updateState, groupId, index }) {
         <Draggable draggableId={card.id} index={index} >
             {(provided) => (
                 <div className="card-preview flex column"  onClick={() => openCardDetails(card.id)} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                    <div>Labels</div>
+                    {/* <div style={{ backgroundColor: "blue" }}>Labels</div> */}
                     <p className="p-card-preview">{card.title}</p>
                     {card.imgUrl && <img className="img-card-preview" src={card.imgUrl} alt="Loading"/>}
                 </div>
