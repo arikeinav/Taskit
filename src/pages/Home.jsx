68d7d62aac77774348cb88a59c86bc9085db6151
Shlomikop <64078705+Shlomikop@users.noreply.github.com>
2020-09-21 18:36:33 +0300

    123

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,11 @@ import { Link } from 'react-router-dom'
 
 export function Home() {
     return (
-        <div className="home flex column align-center justify-center">
+        <div className="home flex column ">
             <h1>Welcome To Task<span className="logo-i">i</span>t</h1>
+            <pre>Organizing your</pre><pre>daily tasks</pre>
+                
+           
             <Link to="/board"><p>Start</p></Link>
         </div>
     )
