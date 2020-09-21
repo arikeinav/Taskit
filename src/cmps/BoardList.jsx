f40c9f98a0a1b0e0843a6e9e9ee7339a0bb43981
arikeinav <68658683+arikeinav@users.noreply.github.com>
2020-09-21 14:35:50 +0300

    rdg

--- src/cmps/BoardList.jsx
+++ src/cmps/BoardList.jsx
@@ -6,7 +6,7 @@ export function BoardList({boards,onAddBoard}) {
     return <div className="board-list grid">
          
            { boards.map(board => <BoardPreview  board={ board } key={ board._id } />)}
-       <div style={{backgroundColor:'gray'}} onClick={onAddBoard}  className="board-preview grid">
+       <div style={{backgroundColor:'gray'}} onClick={onAddBoard}  className="board-preview  grid">
           <h3>Create</h3> 
         </div>
         
--- src/cmps/BoardPreview.jsx
+++ src/cmps/BoardPreview.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function BoardPreview({board}) {
-
-
-    return (
-       <div><Link to={`/board/${board._id}`}>
-       <div style= {board.style.bgImg?{backgroundImage:`url(${board.style.bgImg})`,backgroundSize:"cover",backgroundRepeat: "no-repeat" }:{backgroundColor:`${board.style.bgColor?board.style.bgColor:'green'}`}} className="board-preview grid">
-          <h3>{board.title}</h3> 
+export default function BoardPreview({ board }) {
+  return (
+    <div>
+      <Link to={`/board/${board._id}`}>
+        <div
+          style={
+            board.style.bgImg
+              ? {
+                  backgroundImage: `url(${board.style.bgImg})`,
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                }
+              : {
+                  backgroundColor: `${
+                    board.style.bgColor ? board.style.bgColor : "green"
+                  }`,
+                }
+          }
+          className="board-preview grid"
+        ><div className="board-preview-title-div">
+        <h3>{board.title}</h3><div className="board-preview-fake-div">
+            
+            </div>
+      </div>
+          
         </div>
-        </Link></div>
-    )
+      </Link>
+    </div>
+  );
 }
