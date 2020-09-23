a03216b995b0cbc1cec9cfe53135917133cf8a99
Shahar Sadof <shaharsadof22@gmail.com>
2020-09-23 12:54:07 +0300

    aaa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,8 +15,28 @@ export function Home() {
             </div>
             <div className="home-details-comtainer">
                 <h1>THE POWER OF TASKIT</h1>
-                <section>
-                    
+                <section className="home-details flex">
+                    <img src="https://res.cloudinary.com/cloudinary-img/image/upload/v1600352679/Taskit/tony-yeung-Exq6e9gH4Ag-unsplash_e7ltjx.jpg" />
+                    <div className="text-in-home-details">
+                        <h3>Web Sockets</h3>
+                        <p>With the power of web sockets, you and your team can enjoy a workspace that updates in real-time, keeping everyone synced and up to date.</p>
+                    </div>
+                </section>
+
+                <section className="home-details flex">
+                    <div>
+                        <h3>Drag n Drop!</h3>
+                        <p>Try our Drag n Drop technology to easily move & update your tasks with your mouse or finger.</p>
+                    </div>
+                    <img src="https://res.cloudinary.com/cloudinary-img/image/upload/v1600352679/Taskit/tony-yeung-Exq6e9gH4Ag-unsplash_e7ltjx.jpg" />
+                </section>
+
+                <section className="home-details flex">
+                    <img src="https://res.cloudinary.com/cloudinary-img/image/upload/v1600352679/Taskit/tony-yeung-Exq6e9gH4Ag-unsplash_e7ltjx.jpg" />
+                    <div>
+                        <h3>Make it yours.</h3>
+                        <p>Make it feel like home for you and your team. Customize the board's appearance, Organize your lists to fit the team's flow.</p>
+                    </div>
                 </section>
             </div>
         </div>
