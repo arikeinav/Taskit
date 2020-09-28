ef6e9afcb701bcd4dca9f7690eb881d50c009b6d
Shlomikop <64078705+Shlomikop@users.noreply.github.com>
2020-09-28 15:58:05 +0300

    123

--- src/cmps/ChecklistPreview.jsx
+++ src/cmps/ChecklistPreview.jsx
@@ -142,7 +142,7 @@ export class _ChecklistPreview extends Component {
                 </div>
                 <label htmlFor="progress-bar">Todos progress: {this.state.progressbar}%</label>
                 <progress style={{ width: '50%' }} id="progress-bar" value={`${this.state.progressbar}`} max="100"></progress>
-                <p className="cd-subt" style={{ fontSize: '0.95rem'}}>Your Todos:</p>
+                <p className="cd-subt" style={{ marginTop:'25px', fontSize: '0.95rem'}}>Your Todos:</p>
                 <div className="grey-zone">
                     {checklist.todos && checklist.todos.map(todo => <TodoPreview key={todo.id} todo={todo} updateCheckbox={this.updateLocalChecklist} onRemoveTodo={this.onRemoveTodo} />)}
                     <button style={{
