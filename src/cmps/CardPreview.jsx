0fb6bc88149c88cf773a516e6281e841d7511f8c
Shlomikop <64078705+Shlomikop@users.noreply.github.com>
2020-09-28 15:03:06 +0300

    123

--- src/cmps/CardPreview.jsx
+++ src/cmps/CardPreview.jsx
@@ -27,7 +27,7 @@ export function CardPreview({ card, updateState, groupId, index, calcProgress })
                 >
                     {style => (
                         <Container   isDragging={snapshot.isDragging}  className="card-preview flex column justify-center" onClick={() => openCardDetails(card.id)} {...provided.draggableProps} {...provided.dragHandleProps} style={style} ref={provided.innerRef}>
-                            <div className='card' style={{ backgroundColor: card.bgColor }} >
+                            <div className='card flex column space-between' style={{ backgroundColor: card.bgColor }} >
                                 <div className="flex  space-between">
                                 <div className="flex">{(card.labels && card.labels.length > 0) && 
                                         card.labels.map(label => <div key={label} className="small-small-label" style={{ backgroundColor: label }} />)
@@ -35,7 +35,7 @@ export function CardPreview({ card, updateState, groupId, index, calcProgress })
                                     {card.checklist && <div className="footer-div-card-prev">{calcProgress(card.id)}</div>}
                                 </div>
                                 <p className="p-card-preview">{card.title}</p>
-                                        {card.youtube && <ReactPlayer width='100%' height='100%' url={card.youtube} /> ||
+                                        {card.youtube && <div className="img-card-preview"><ReactPlayer  width='100%' height='100%' url={card.youtube} /></div>  ||
                                         card.imgUrl && <img className="img-card-preview" src={card.imgUrl} alt="Loading" />}
                             </div>
                             
