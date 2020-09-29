82b59342dcf1c7850bbb8e2c714b6634d59ca26a
Shahar Sadof <shaharsadof22@gmail.com>
2020-09-29 20:41:01 +0300

    end day!

--- src/cmps/CardDetails.jsx
+++ src/cmps/CardDetails.jsx
@@ -363,17 +363,17 @@ export class _CardDetails extends Component {
                         </Element>
 
                         <div className="side-bar-details-right flex column justify-start">
-                            <button className="details-btn align-center" style={{ display: 'flex' }} onClick={() => this.updateState('isAddImgModalShown', true)}><FaFileImage style={{ marginRight: "7px" }} />Cover</button>
+                            <button className="btn align-center" style={{ display: 'flex' }} onClick={() => this.updateState('isAddImgModalShown', true)}><FaFileImage style={{ marginRight: "7px" }} />Cover</button>
 
-                            <button className="details-btn align-center" style={{ display: 'flex' }} onClick={this.onOpenColorModal}><MdColorLens style={{ marginRight: "3px", height: '12px', width: '12px' }} />Color</button>
+                            <button className="btn align-center" style={{ display: 'flex' }} onClick={this.onOpenColorModal}><MdColorLens style={{ marginRight: "3px", height: '12px', width: '12px' }} />Color</button>
 
-                            <button className="details-btn align-center" style={{ display: 'flex' }} onClick={() => this.openChecklistEditor()}><FaCheckCircle style={{ marginRight: "6px" }} />Checklist</button>
-                            <button onClick={this.onOpenDuedate} className="details-btn align-center"><FaCalendarAlt style={{ marginRight: "5px" }} /> Duedate</button>
-                            <button onClick={this.onOpenLabelModal} style={{ display: 'flex' }} className="details-btn align-center"><MdInvertColors style={{ marginRight: "5px", height: '12px', width: '12px' }} />Labels</button>
+                            <button className="btn align-center" style={{ display: 'flex' }} onClick={() => this.openChecklistEditor()}><FaCheckCircle style={{ marginRight: "6px" }} />Checklist</button>
+                            <button onClick={this.onOpenDuedate} className="btn align-center"><FaCalendarAlt style={{ marginRight: "5px" }} /> Duedate</button>
+                            <button onClick={this.onOpenLabelModal} style={{ display: 'flex' }} className="btn align-center"><MdInvertColors style={{ marginRight: "5px", height: '12px', width: '12px' }} />Labels</button>
                             {this.state.isLabelesEdit &&
                                 <ColorModal className="color-modal" onSaveLabels={this.onSaveLabels} labels={card.labels} />}
 
-                            <button className="details-btn align-center" style={{ display: 'flex' }} onClick={() => (this.setState({ isYoutubeShown: true }))}><FaYoutube style={{ marginRight: "6px", height: '12px', width: '12px' }} /> YouTube</button>
+                            <button className="btn align-center" style={{ display: 'flex' }} onClick={() => (this.setState({ isYoutubeShown: true }))}><FaYoutube style={{ marginRight: "6px", height: '12px', width: '12px' }} /> YouTube</button>
                             {this.state.isYoutubeShown && <div style={{ border: "1px solid black" }}><EditableLabel
                                 initialValue={'Paste Url'}
                                 save={(url) => this.youtubeFunc(url)}
@@ -381,8 +381,8 @@ export class _CardDetails extends Component {
                                 inputClassName='youtube-input'
                             /></div>
                             }
-                            {!card.youtube && <button onClick={() => this.setState({ isCanvas: true })} className="details-btn"><FaPaintBrush style={{ marginRight: "6px" }} />Canvas</button>}
-                            <button onClick={this.onHandleRemove} className="details-btn"> <FaTrashAlt style={{ marginRight: "6px" }} />Card</button>
+                            {!card.youtube && <button onClick={() => this.setState({ isCanvas: true })} className="btn"><FaPaintBrush style={{ marginRight: "6px" }} />Canvas</button>}
+                            <button onClick={this.onHandleRemove} className="btn"> <FaTrashAlt style={{ marginRight: "6px" }} />Card</button>
                         </div>
 
                     </div >
